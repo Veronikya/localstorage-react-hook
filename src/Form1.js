11318dad661fe3019ae092d9cefe8a983665c945
Veronikya <yuroocky@gmail.com>
2022-01-25 20:30:54 +0800

    https://github.com/Veronikya/useLocalStorage.git

---
+++ src/Form1.js
@@ -0,0 +1,17 @@
+import { useState,useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+export default function Form1(){
+    const [name, setName] = useLocalStorage("name", "");
+  return (
+    <form>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Full name"
+        aria-label="fullname"
+      />
+      <input type="submit" value="Submit"></input>
+    </form>
+  );
+}
