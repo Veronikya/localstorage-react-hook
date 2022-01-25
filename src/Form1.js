36dd8578f84be6cbeb7ef463a9929774cb37f3b8
Veronikya <yuroocky@gmail.com>
2022-01-25 20:35:54 +0800

    https://github.com/Veronikya/useLocalStorage

--- src/Form1.js
+++ src/Form1.js
@@ -2,6 +2,7 @@ import { useState,useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 export default function Form1(){
     const [name, setName] = useLocalStorage("name", "");
+    console.log()
   return (
     <form>
       <input
